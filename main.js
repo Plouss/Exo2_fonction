620298a8ca4525702160e39b25ad3eaf2068d236
Plouss <80962741+Plouss@users.noreply.github.com>
2021-04-29 18:48:47 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,34 @@
+// //1
+// function reverseString(str) {
+
+//     return parseInt(str.toString().split("").reverse().join(""));
+// }
+// console.log(reverseString(851)); 
+
+// //2
+// function divPar2(number) {
+//     switch (number % 2) {
+//         case 0:
+//             return console.log(`Le ${number} est divisible par 2. ${number}:2 = ${number/2}`);
+//         case 1:
+//             return console.log("Ce nombre n'est pas divisible par 2");
+//         default:
+//             return console.log("ceci n'est pas un nombre");
+//     }
+// }
+
+// divPar2(8);
+
+// //exo3
+// function logIn() {
+//     let mdp;
+//     while (mdp != "mdp") {
+//         mdp = prompt('Votre mot de passe :');
+//     }
+//     alert("Bravo");
+// }
+
+// logIn();
+
+
+
